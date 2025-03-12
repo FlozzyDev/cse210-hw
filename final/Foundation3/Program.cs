0b55cca0d85f34630e8d4c41fcb8fab5787f3c0f
FlozzyDev <dallinhale@gmail.com>
2025-03-12 16:08:23 -0600

    Changed the details so it made more sense, 3 events on the same day in the same region

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,12 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Address address1 = new Address("123 Main St", "Rexburg", "ID", "12345");
-        Address address2 = new Address("456 Elm St", "Redding", "CA", "67890");
-        Address address3 = new Address("7877 Oak St", "Boulder", "CO", "13579");
+        Address address1 = new Address("510 S Center St. (Room 346)", "Rexburg", "ID", "83460");
+        Address address2 = new Address("859 S Yellowstone Hwy #1002", "Rexburg", "ID", "83440");
+        Address address3 = new Address("S 2nd W & 3rd W,", "Rexburg", "ID", "83440");
         LectureEvent lecture1 = new LectureEvent("Lecture on C#", "Learn about C# programming", "3/15/2025", "10:00 AM", address1, "Lecture", "John Doe", 100);
-        ReceptionEvent wedding1 = new ReceptionEvent("Wedding: Tom and Jill", "Come celebrate the marriage of Tom and Jill!", "3/15/2025", "6:00 PM", address2, "Reception", "eventrsvp@hotmail.com");
-        OutdoorEvent concert1 = new OutdoorEvent("Outdoor Concert", "Live music in the park", "3/15/2025", "7:00 PM", address3, "Concert", "Sunny");
+        ReceptionEvent wedding1 = new ReceptionEvent("Wedding: Tom and Jill", "Come (if you RSVP'ed) and celebrate the marriage of Tom and Jill!", "3/15/2025", "3:00 PM", address2, "Reception", "eventrsvp@hotmail.com");
+        OutdoorEvent concert1 = new OutdoorEvent("Outdoor Concert: Porter Park Polka", "Live music in the park, come dance!", "3/15/2025", "7:00 PM", address3, "Concert", "Cloudy");
 
         Console.WriteLine("----------Upcoming Events for 3/15/2025!----------");
         Console.WriteLine();
